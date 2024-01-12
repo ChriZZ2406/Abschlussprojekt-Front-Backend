b56fff670bf75c32a55481f6673c0176ddcdd0ef
Chris <christian.altenhofer@gmx.de>
2024-01-12 11:10:42 +0100

    Logo Einbindung

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* Man fügt das Logo hier ein */}
-        <img src="/pfad/zum/logo.png" alt="Logo" />
+        <img src="https://cdn.discordapp.com/attachments/1195301143161606205/1195301598507827240/techst_logo_rz_white.png?ex=65b37e5c&is=65a1095c&hm=951cba6cabd865ab2f4e7c4fd8e295c18bb4f3b9a3474d434849184a84fcbd48&" alt="Logo" className="logo" />
       </header>
       <div className="App-body">
         {/* Man fügt die Buttons für Login und Account-Erstellung hinzu */}
