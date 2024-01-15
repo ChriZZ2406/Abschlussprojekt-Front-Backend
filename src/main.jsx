ac9168083a21563c0b9e89451f81bddf8b500eed
Chris <christian.altenhofer@gmx.de>
2024-01-15 22:09:50 +0100

    15.01.2024 - 22.10 Uhr GoogleLogin / Create Account, Fix ausstehend

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+//##################### routes
+// import
+// import
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//########## Was soll der Part und was handelt dieser? RS Karim #############
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//#################################routen
+import Root from './routes/roote';
+import LoginGoogle from './routes/logingoogle';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/login/google",
+    element: <LoginGoogle />,
+  },
+]);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
