aacabb877ef8a54c6bc46fc1c329b93faf5880bf
Steven <steven.albrecht@docc.techstarter.de>
2024-01-17 14:48:05 +0100

    nab 17.01.24 14:50

---
+++ src/routes/nab.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+
+// Man definiert eine Button-Komponente für die Erstellung eines neuen Kontos
+const NewAccountButton = ({ onClick }) => {
+  return (
+    <button onClick={onClick}>
+      New Account
+    </button>
+  );
+}
+
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleCreateAccount = () => {
+    console.log('Account erstellen:', { username, email, password, confirmPassword });
+  };
+
+  return (
+    <div className="app">
+      <div className="signup-container">
+        <img src="dein-logo.png" alt="Logo" className="logo" />
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Mailadresse"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Passwort"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Passwort wiederholen"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button onClick={handleCreateAccount}>Erstellen</button>
+      </div>
+    </div>
+  );
+}
+
+export default NewAccountButton;
