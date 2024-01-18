eff29d4593693662972c3a279166c5d7b5334962
marcoruz <marco.ruzzo@docc.techstarter.de>
2024-01-18 23:41:33 +0100

    18.01.24 23:40 Uhr Chat Sprechblase mit Schweif

--- src/routes/newsfeed.jsx
+++ src/routes/newsfeed.jsx
@@ -12,7 +12,20 @@ const App = () => {
       setNewMessage('');
     }
   };
+  const UserInfo = ({ user }) => (
+    <div className="user-info">
+      {user.name} ({user.abbreviation})
+    </div>
+   );
 
+   {messages.map((message, index) => (
+    <div key={index} className="message">
+      <UserInfo user={message.user} />
+      {message.text}
+    </div>
+   ))}
+   
+   
   return (
     <div className="app">
       <img src="https://cdn.discordapp.com/attachments/1195301143161606205/1195301598507827240/techst_logo_rz_white.png?ex=65b37e5c&is=65a1095c&hm=951cba6cabd865ab2f4e7c4fd8e295c18bb4f3b9a3474d434849184a84fcbd48&" alt="Logo" className="logo" />
