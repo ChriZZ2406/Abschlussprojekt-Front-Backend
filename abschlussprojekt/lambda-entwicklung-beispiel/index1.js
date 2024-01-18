dca3b3c21e79f7076608a31af1191e66c2793149
AndiBruehl <a.bruehl2019@gmail.com>
2024-01-18 15:57:14 +0100

    changed index1.js

--- abschlussprojekt/lambda-entwicklung-beispiel/index1.js
+++ abschlussprojekt/lambda-entwicklung-beispiel/index1.js
@@ -11,11 +11,11 @@ const apiEvent = {
     token: "",
   },
   body: {
-    id: "10710972933354",
-    email: "karim.aouini@docc.techstarter.de",
-    name: "Karim Aouini",
-    given_name: "Karim\t",
-    family_name: "Aouini",
+    id: "109",
+    email: "andreas.bruehl@docc.techstarter.de",
+    name: "Andreas Brühl",
+    given_name: "Andreas\t",
+    family_name: "Brühl",
     picture:
       "https://lh3.googleusercontent.com/a/ACg8ocIqaBD6Ind3rDHkOYM1ZBrZtCgEfAe6IRaZDrj7ERc=s96-c",
   },
@@ -38,17 +38,32 @@ const main = async () => {
 
     // Nutzer hinzufügen
     const ed = apiEvent.body;
-    await sequelize.query(`
-      INSERT INTO User (UserID, RealName, EmailAddress, BirthDate, Course, AuthProvider, ProfileImg)
-      VALUES ('${ed.id}', '${ed.name}', '${ed.email}', null, null, null, '${ed.picture}')
-      ON DUPLICATE KEY UPDATE
-        UserID = VALUES(UserID),
-        RealName = VALUES(RealName),
-        BirthDate = VALUES(BirthDate),
-        Course = VALUES(Course),
-        AuthProvider = VALUES(AuthProvider),
-        ProfileImg = VALUES(ProfileImg);
+    
+    // Check if user already exists in the database
+    const [existingUser, _] = await sequelize.query(`
+      SELECT * FROM User WHERE UserID = '${ed.id}'
     `);
+
+    if (existingUser.length > 0) {
+      // User is already in the database
+      console.log('User vorhanden');
+    } else {
+      // User is not in the database, add new entry
+      await sequelize.query(`
+        INSERT INTO User (UserID, RealName, EmailAddress, BirthDate, Course, AuthProvider, ProfileImg)
+        VALUES ('${ed.id}', '${ed.name}', '${ed.email}', null, null, null, '${ed.picture}')
+        ON DUPLICATE KEY UPDATE
+          UserID = VALUES(UserID),
+          EmailAddress = VALUES(EmailAddress),
+          RealName = VALUES(RealName),
+          BirthDate = VALUES(BirthDate),
+          Course = VALUES(Course),
+          AuthProvider = VALUES(AuthProvider),
+          ProfileImg = VALUES(ProfileImg);
+      `);
+
+      console.log('Neuer Eintrag in der Datenbank erstellt');
+    }
 
     const [results, metadata] = await sequelize.query("SELECT * FROM User");
     console.log(results);
